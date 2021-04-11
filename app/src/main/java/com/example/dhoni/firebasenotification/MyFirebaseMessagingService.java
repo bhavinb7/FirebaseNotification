052821104e1255206c44bdb0765cfe58cba20025
bhavinb7 <bhavinbalwani31@gmail.com>
2021-04-11 13:56:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dhoni/firebasenotification/MyFirebaseMessagingService.java
@@ -0,0 +1,30 @@
+package com.example.dhoni.firebasenotification;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        getFirebasemessage(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+    }
+
+    public void getFirebasemessage(String title, String msg){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"mychannel")
+                .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setAutoCancel(true);
+
+
+        NotificationManagerCompat manager = NotificationManagerCompat.from(this);
+        manager.notify(101,builder.build());
+
+    }
+}
